c404ce8e79cf142ffb8e9dd32766822328ae4010
SZigmund <szigmund@hotmail.com>
2015-09-28 19:13:43 -0400

    Update noVideo.js

--- noVideo.js
+++ noVideo.js
@@ -2,36 +2,36 @@
 var noVideo = {
 	started: false,
 	version: "00.02"
-};
-logException: function(msg) {  // Log exception to console
+}
+noVideo.logException: function(msg) {  // Log exception to console
   try {   console.log("ERROR: " + msg);}
   catch(err) { console.log("ERROR:logException: " + err.message); }
 };
-undisplayElement = function(elementName) {
+noVideo.undisplayElement = function(elementName) {
 	try {
 	   document.getElementById(elementName).style.display = 'none';
         }
         catch(err) {
-           logException("undisplayElement: " + err.message);
+           noVideo.logException("undisplayElement: " + err.message);
         }
 };
-hideElement = function(elementName) {
+noVideo.hideElement = function(elementName) {
 	try {
 	   document.getElementById("elementName").style.visibility = "hidden";
         }
         catch(err) {
-           logException("hideElement: " + err.message);
+           noVideo.logException("hideElement: " + err.message);
         }
 };
 noVideo.init = function(){
   //var elem = document.getElementById("main_player");
   //elem.parentNode.removeChild(elem);
-  hideElement("ytp-player-content ytp-iv-player-content");
-  hideElement("main_player");
-  hideElement("left_section");
-  undisplayElement("ytp-player-content ytp-iv-player-content");
-  undisplayElement("main_player");
-  undisplayElement("left_section");
+  noVideo.hideElement("ytp-player-content ytp-iv-player-content");
+  noVideo.hideElement("main_player");
+  noVideo.hideElement("left_section");
+  noVideo.undisplayElement("ytp-player-content ytp-iv-player-content");
+  noVideo.undisplayElement("main_player");
+  noVideo.undisplayElement("left_section");
 };
 
 if (!noVideo.started) noVideo.init();
