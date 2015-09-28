d67bd727d8e75b710ff9cfec706391dd740a4535
SZigmund <szigmund@hotmail.com>
2015-09-28 18:58:53 -0400

    Update noVideo.js

--- noVideo.js
+++ noVideo.js
@@ -3,13 +3,35 @@ var noVideo = {
 	started: false,
 	version: "00.02"
 }
-
+logException: function(msg) {  // Log exception to console
+  try {   console.log("ERROR: " + msg);}
+  catch(err) { console.log("ERROR:logException: " + err.message); }
+}
+undisplayElement = function(elementName) {
+	try {
+	   document.getElementById(elementName).style.display = 'none';
+        }
+        catch(err) {
+           logException("undisplayElement: " + err.message);
+        }
+}
+hideElement = function(elementName) {
+	try {
+	   document.getElementById("elementName").style.visibility = "hidden";
+        }
+        catch(err) {
+           logException("hideElement: " + err.message);
+        }
+}
 noVideo.init = function(){
   //var elem = document.getElementById("main_player");
   //elem.parentNode.removeChild(elem);
-  document.getElementById("main_player").style.visibility = "hidden";
-  document.getElementById("left_section").style.visibility = "hidden";
-  document.getElementById("ytp-player-content ytp-iv-player-content").style.visibility = "hidden";
+  hideElement("ytp-player-content ytp-iv-player-content");
+  hideElement("main_player");
+  hideElement("left_section");
+  undisplayElement("ytp-player-content ytp-iv-player-content");
+  undisplayElement("main_player");
+  undisplayElement("left_section");
 };
 
 if (!noVideo.started) noVideo.init();
