78e242c32ecb09053d27fec790cd6c28ee2f1285
SZigmund <szigmund@hotmail.com>
2015-09-28 18:43:38 -0400

    Update noVideo.js

--- noVideo.js
+++ noVideo.js
@@ -8,6 +8,7 @@ noVideo.init = function(){
   //var elem = document.getElementById("main_player");
   //elem.parentNode.removeChild(elem);
   document.getElementById("main_player").style.visibility = "hidden";
+  document.getElementById("left_section").style.visibility = "hidden";
 };
 
 if (!noVideo.started) noVideo.init();
