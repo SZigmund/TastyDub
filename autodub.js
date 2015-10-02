c113ac34e30baf76081d49bd385f343db9395e82
SZigmund <szigmund@hotmail.com>
2015-10-02 16:47:24 -0400

    Create autodub.js

---
+++ autodub.js
@@ -0,0 +1,41 @@
+~function(){
+    // initialization
+    //https://raw.githubusercontent.com/nderscore/DubtrackBookmarklets/master/autodub.js
+    //https://github.com/nderscore/DubtrackBookmarklets/blob/master/autodub.js
+    if(!window._autodub){
+        Dubtrack.Events.bind("realtime:room_playlist-update", function(){  
+            window._autodubdownvote = null;
+            if(window._autodub == 'on'){
+                setTimeout(function(){
+                    if(window._autodubdownvote){
+                        return;
+                    }
+                    $('.dubup').trigger('click');
+                }, 30e3 + (Math.random() * 30e3 | 0));
+            }
+        });
+
+        $('.dubdown').bind('click', function(){
+            window._autodubdownvote = true;
+        });
+    }
+
+    var $chatmain = $('.chat-main');
+
+    // toggle
+    if(window._autodub == 'on'){
+        window._autodub = 'off';
+        window._autodubdownvote = true;
+        $chatmain.append('<li class="_autodub">_nderscore\'s autodub disabled!</li>');
+    } else {
+        window._autodub = 'on';
+        $chatmain.append('<li class="_autodub">_nderscore\'s autodub enabled!</li>');
+        $('.dubup').trigger('click');
+    }
+
+    Dubtrack.room.chat.scollBottomChat();
+
+    setTimeout(function(){ 
+        $('._autodub').remove(); 
+    }, 3e3);
+}();
