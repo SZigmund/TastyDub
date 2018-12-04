ce04e367f537acf4f3045f953ea838066f7b4628
SZigmund <szigmund@hotmail.com>
2018-12-04 13:12:36 -0500

    Update and rename PortDubToPlug4.js to PortDubToPlug1.js

--- PortDubToPlug1.js
+++ PortDubToPlug1.js
@@ -117,21 +117,22 @@
 
 	
 	var DUBPlaylists = [];
+	PLUG.importSongs();
 })();
 
-(function(){
-	var ow = {
-		main : {
-			init : function(){
-				//ow.main.addEvents();
-				PLUG.importSongs();
-			},
-			addEvents: function(){
-				//$('.community__name').click(PLUG.importSongs);  	//Plug
-				$('.community__name').click(console.log("name"));  	//Plug
-				$('.community__meta').click(console.log("meta"));  	//Plug
-			},
-		}
-	};
-	ow.main.init();
-})();
+//(function(){
+//	var ow = {
+//		main : {
+//			init : function(){
+//				//ow.main.addEvents();
+//				PLUG.importSongs();
+//			},
+//			addEvents: function(){
+//				//$('.community__name').click(PLUG.importSongs);  	//Plug
+//				$('.community__name').click(console.log("name"));  	//Plug
+//				$('.community__meta').click(console.log("meta"));  	//Plug
+//			},
+//		}
+//	};
+//	ow.main.init();
+//})();
